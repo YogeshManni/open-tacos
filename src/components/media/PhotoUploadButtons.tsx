f4ff7aaf556a7a9450511c198c8a1481f0f883dc
Yogesh Manni <yogeshmanni786@gmail.com>
2024-03-28 09:04:54 -0700

    Add status condition in  uploadphoto component to enable or disable the button

--- src/components/media/PhotoUploadButtons.tsx
+++ src/components/media/PhotoUploadButtons.tsx
@@ -1,9 +1,8 @@
-
 import { Camera } from '@phosphor-icons/react/dist/ssr'
 import { BaseUploaderWithNext13Context } from './BaseUploader'
 
 export const UploadPhotoButton: React.FC<{status:string}> = ({status}) => (
-  <BaseUploaderWithNext13Context className='btn'>
+  <BaseUploaderWithNext13Context className={`btn ${status === 'unauthenticated' && 'pointer-events-none opacity-50'}`}>
     <Camera size={20} /> <span className='hidden md:inline'>Photo</span>
   </BaseUploaderWithNext13Context>
 )
